ff921a3e9e603379450bd9ced7112966eeb14ad2
Antonio Ferrandiz <u201918042@upc.edu.pe>
2021-05-06 15:52:12 -0500

    001

---
+++ HeartHome.Web/Controllers/PropertyController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using HeartHome.Data;
+using HeartHome.Entities;
+using HeartHome.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HeartHome.Web.Controllers
+{
+    [Route("api/[Controller]")]
+    [ApiController]
+    public class PropertyController : ControllerBase
+    {
+        private readonly DbContextHeartHomeApp _context;
+        public PropertyController(DbContextHeartHomeApp context)
+        {
+            _context = context;
+        }
+
+        [HttpGet()]
+        public async Task<IEnumerable<PropertyModel>> GetProperties()
+        {
+            var propertyList = await _context.Properties.ToListAsync();
+            return propertyList.Select(c => new PropertyModel
+            {
+                PropertyID = c.PropertyID,
+                CityID = c.CityID,
+                LessorID = c.LessorID,
+                Type = c.Type,
+                Address = c.Address,
+                Name = c.Name,
+                Phone = c.Phone
+
+            });
+        }
+    }
+}
---
+++ HeartHome.Web/Models/PropertyModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace HeartHome.Web.Models
+{
+    public class PropertyModel
+    {
+        public int PropertyID { get; set; }
+        public int CityID { get; set; } //FK
+        public int LessorID { get; set; } //FK
+        public string Type { get; set; }
+        public string Address { get; set; }
+        public string Name { get; set; }
+        public int Phone { get; set; }
+    }
+}
