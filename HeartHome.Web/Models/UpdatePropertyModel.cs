6a1207a710fdf2ee8bc6f6a4d3db8bdcf37184da
Antonio Ferrandiz <u201918042@upc.edu.pe>
2021-05-07 03:03:35 -0500

    003

---
+++ HeartHome.Web/Models/UpdatePropertyModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HeartHome.Web.Models
+{
+    public class UpdatePropertyModel
+    {
+        public int PropertyID { get; set; }
+
+        [Required(ErrorMessage = "Debe ingresar el tipo de propiedad.")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "El tipo de propiedad debe tener de 3 a 50 caracteres")]
+        public string Type { get; set; }
+
+        [Required(ErrorMessage = "Debe ingresar la dirección de la propiedad.")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "La dirección debe tener de 3 a 50 caracteres")]
+        public string Address { get; set; }
+
+        [Required(ErrorMessage = "Debe ingresar el nombre de la propiedad.")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "El nombre de la propiedad debe tener de 3 a 50 caracteres")]
+        public string Name { get; set; }
+    }
+}
